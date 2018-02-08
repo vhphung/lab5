9a98f50e457dd789b5cb445c1cf16e59f97d0124
vhphung <vhphung@ucsd.edu>
2018-02-08 02:04:04 -0800

    lab 5 completed

--- public/js/hci-friends.js
+++ public/js/hci-friends.js
@@ -10,6 +10,7 @@ $(document).ready(function() {
  */
 function initializePage() {
 	console.log("Javascript connected!");
+	$("a").click(nameClick);
 }
 
 function anagrammedName(name) {
@@ -43,4 +44,10 @@ function anagrammedName(name) {
 		console.log(name + " not known for anagramming.");
 		return name;
 	}
+	
+}
+
+function nameClick(e){
+	e.preventDefault();
+	$(this).text(anagrammedName($(this).text()));
 }
